6052621b41347f7ffce92c5577740c5277d7619c
Oskar Dudycz <oskar.dudycz@gmail.com>
2017-11-05 18:20:18 +0100

    Added helper batch scripts

---
+++ run.sh
@@ -0,0 +1,1 @@
+docker exec -t -i docker_spark-master_1 bash -c "spark-submit --class sparkWithScalaAndDocker.FileLinesCount /app/target/scala-2.11/sparkwithscalaanddocker_2.11-0.1.jar /app/build.sbt"
